65d936c0c88c77bf5a19773fcddb2cef27bd5314
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-13 20:44:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,30 +7,30 @@ const formData = {};
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   localStorage.removeItem(KEY_STORAGE);
-//   e.currentTarget.reset();
-// }
+function onFormSubmit(e) {
+  e.preventDefault();
+  localStorage.removeItem(KEY_STORAGE);
+  e.currentTarget.reset();
+}
 
-// function onFormInput(e) {
-// formData[e.target.name] = e.target.value;
-// localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
-// }
+function onFormInput(e) {
+formData[e.target.name] = e.target.value;
+localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
+}
 
-// function populateTextarea() {
-//     let formData = localStorage.getItem(KEY_STORAGE);
-//     if (formData === null) {
-//         try {
-//             formData = JSON.parse(formData);
-//             form.elements.email.value = formData.email;
-//             form.elements.message.value = formData.message;
-//         }
-//         catch (error) {
-//             console.error(
-//                 'Error: invalid saved form state in LocalStorage!' + KEY_STORAGE
-//             );
-//             console.error(error);
-//         }
-//     }
-// }
+function populateTextarea() {
+    let formData = localStorage.getItem(KEY_STORAGE);
+    if (formData === null) {
+        try {
+            formData = JSON.parse(formData);
+            form.elements.email.value = formData.email;
+            form.elements.message.value = formData.message;
+        }
+        catch (error) {
+            console.error(
+                'Error: invalid saved form state in LocalStorage!' + KEY_STORAGE
+            );
+            console.error(error);
+        }
+    }
+}
