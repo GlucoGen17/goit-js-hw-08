ce1ad4116aad60a7d6eb75338d1ca738f91165fd
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-13 20:12:13 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,10 +2,8 @@ import throttle from 'lodash.throttle';
 
 const KEY_STORAGE = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector('.feedback-form input');
-const massege = document.querySelector('.feedback-form textarea');
 
-let formData = {};
+const formData = {};
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
@@ -15,6 +13,23 @@ function onFormSubmit(e) {
   e.currentTarget.reset();
 }
 
+function onFormInput(e) {
+formData[e.target.name] = e.target.value;
+localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
+}
 
-
-
+function populateTextarea() {
+    let formData = localStorage.getItem(KEY_STORAGE);
+    if (formData !== null) {
+        try {
+            formData = JSON.parse(formData);
+            form.elements.email.value = formData.email;
+            form.elements.message.value = formData.message;
+        }
+        catch (error) {
+            console.error(
+                'Error: invalid saved form state in LocalStorage!' + KEY_STORAGE
+            );
+            console.error(error);
+        }
+    }
