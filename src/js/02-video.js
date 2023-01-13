1be5035bacc009b971351b9274228e834d333055
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-13 20:51:49 +0200

    all

---
+++ src/js/02-video.js
@@ -0,0 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_PLAY_TIME = 'videoplayer-current-time';
+    
+player.on('timeupdate', throttle(onTime, 1000));
+
+function onTime(currentTime) {
+    localStorage.setItem(KEY_PLAY_TIME, JSON.stringify(currentTime.seconds));
+    };
+
+onSavedTime();
+function onSavedTime() {
+  //   const savedTime = JSON.parse(localStorage.getItem(KEY_PLAY_TIME));
+  const savedTime = localStorage.getItem(KEY_PLAY_TIME);
+  //   if (savedTime) {
+  if (!savedTime) {
+    return;
+  }
+  player.setCurrentTime(savedTime);
+}
---
+++ src/js/03-feedback.js
@@ -0,0 +1,36 @@
+import throttle from 'lodash.throttle';
+
+const KEY_STORAGE = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+const formData = {};
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  localStorage.removeItem(KEY_STORAGE);
+  e.currentTarget.reset();
+}
+
+function onFormInput(e) {
+formData[e.target.name] = e.target.value;
+localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+    let formData = localStorage.getItem(KEY_STORAGE);
+    if (formData === null) {
+        try {
+            formData = JSON.parse(formData);
+            form.elements.email.value = formData.email;
+            form.elements.message.value = formData.message;
+        }
+        catch (error) {
+            console.error(
+                'Error: invalid saved form state in LocalStorage!' + KEY_STORAGE
+            );
+            console.error(error);
+        }
+    }
+}
