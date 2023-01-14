f1ce2f19bf19c3c6dfe9c995477e86feea06c785
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-14 19:07:47 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,8 +2,11 @@ import throttle from 'lodash.throttle';
 
 const KEY_STORAGE = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
+
 populateTextarea();
+
 const formData = {};
+
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
